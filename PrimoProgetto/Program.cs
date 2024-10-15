b3df729b875a249f68774eb08c41361bbf7cbc0b
MatteoMant <101134772+MatteoMant@users.noreply.github.com>
2024-10-15 19:01:45 +0200

    Versione 2

--- PrimoProgetto/Program.cs
+++ PrimoProgetto/Program.cs
@@ -1,12 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-#if DEBUG
-    int i = 10;
-#endif
+int i = 10;
 
 #if RELEASE
-    int i = 20;
+    i = 20;
 #endif
 
 for(int j = 1; j <= i; j++)
