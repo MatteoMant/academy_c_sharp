8903e739fec46c9da82d3d0c9f61c84bfe9a905a
MatteoMant <101134772+MatteoMant@users.noreply.github.com>
2024-10-15 18:43:36 +0200

    Primo commit

---
+++ PrimoProgetto/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#if DEBUG
+    int i = 10;
+#endif
+
+#if RELEASE
+    int i = 20;
+#endif
+
+for(int j = 1; j <= i; j++)
+{
+    Console.WriteLine(j.ToString());
+}
